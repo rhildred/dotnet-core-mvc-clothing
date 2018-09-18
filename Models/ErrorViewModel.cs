e93fe1c8f7edbfc40045950eefcca7dc2da0b91e
rhildred <rhildred@gmail.com>
2018-09-18 09:47:46 -0400

    initial commit as scaffolded by dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcClothing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
