7d1800c490a77c88a30073f8338dd87ec81d7a7d
rhildred <rhildred@gmail.com>
2018-09-18 11:50:37 -0400

    Put in the men's page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,11 @@ namespace mvcClothing.Controllers
             return View();
         }
 
+        public IActionResult Mens()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
